06b246393fd60b3a161f9a47e79efd9d810e6ca3
unknown <danielleokia1@gmail.com>
2023-01-10 15:37:49 +0300

    added dependencies, middleware and set up HTML routes

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+
+//setting server up
+const app = express();
+const PORT = process.env.port || 3001;
+
+//middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("./Develop/public"));
+
+//HTML Routes
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
