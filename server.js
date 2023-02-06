289cbc19da03f2640c031d96c82a924d4e0a9a3f
unknown <danielleokia1@gmail.com>
2023-02-07 01:44:35 +0300

    deployment

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const apiRoutes = require('./routes/apiRoutes');
 
 //setting server up
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
+
 
 //middleware
 app.use(express.urlencoded({extended: true}));
